326868197b50d837032bf67be00be0abcb5540ef
Imran Iqbal <iqbalmy@hotmail.com>
2020-11-01 14:44:30 +0000

    ci: extract InSpec `platform` values for each unique platform

--- test/integration/default/controls/_mapdata_spec.rb
+++ test/integration/default/controls/_mapdata_spec.rb
@@ -5,19 +5,22 @@ require 'yaml'
 control '`map.jinja` YAML dump' do
   title 'should match the comparison file'
 
-  # Strip the `platform[:finger]` version number down to the "OS major release"
-  mapdata_file = "_mapdata/#{system.platform[:finger].split('.').first}.yaml"
+  # # Strip the `platform[:finger]` version number down to the "OS major release"
+  # mapdata_file = "_mapdata/#{system.platform[:finger].split('.').first}.yaml"
 
-  # Load the mapdata from profile https://docs.chef.io/inspec/profiles/#profile-files
-  mapdata_dump = YAML.safe_load(inspec.profile.file(mapdata_file))
+  # # Load the mapdata from profile https://docs.chef.io/inspec/profiles/#profile-files
+  # mapdata_dump = YAML.safe_load(inspec.profile.file(mapdata_file))
 
-  # Derive the location of the dumped mapdata
-  output_dir = platform[:family] == 'windows' ? '/temp' : '/tmp'
-  output_file = "#{output_dir}/salt_mapdata_dump.yaml"
+  # # Derive the location of the dumped mapdata
+  # output_dir = platform[:family] == 'windows' ? '/temp' : '/tmp'
+  # output_file = "#{output_dir}/salt_mapdata_dump.yaml"
 
-  describe 'File content' do
-    it 'should match profile map data exactly' do
-      expect(yaml(output_file).params).to eq(mapdata_dump)
-    end
+  # describe file(mapdata_file) do
+  describe '' do
+    it { should eq platform[:family] }
+    it { should eq platform[:name] }
+    it { should eq platform[:release] }
+    it { should eq platform[:arch] }
+    it { should eq platform[:os] }
   end
 end
--- test/integration/default/controls/packages_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-control 'OpenVPN package' do
-  title 'should be installed'
-
-  describe package('openvpn') do
-    it { should be_installed }
-  end
-end
